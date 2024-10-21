19ac49d9023e61b206b6f57d6647dbb3f87c2e82
CircularBox <158319127+CircularBox@users.noreply.github.com>
2024-10-20 20:22:09 -0400

    Two days of debugging and fail condition

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,7 @@ public class Player : MonoBehaviour
 
     public float gravity;
     public float jumpForce = 20;
-    public float groundHeight = -10;
+    public float groundHeight = 40;
     public bool isGrounded = false;
 
     public bool isHoldingJump = false;
@@ -20,6 +20,8 @@ public class Player : MonoBehaviour
     public float topSpeedMaxJumpTime = 0.5f;
     public float jumpTime = 0;
     public float jumpBuffer = 1f; //If the player is close but not quite on the ground, they can still jump
+
+    public bool dead = false;
 
     // Start is called before the first frame update
     void Start()
@@ -54,6 +56,16 @@ public class Player : MonoBehaviour
     {
         Vector2 pos = transform.position;
 
+        if (dead)
+        {
+            return;
+        }
+
+        if (pos.y < -40)
+        {
+            dead = true;
+        }
+
         if (!isGrounded)
         {
             if (isHoldingJump)
@@ -71,24 +83,52 @@ public class Player : MonoBehaviour
                 velocity.y += gravity * Time.fixedDeltaTime;
             }
 
-            Vector2 raycastOrigin = new Vector2(pos.x + .7f, pos.y);
-            Vector2 raycastDirection = Vector2.up;
-            float raycastDistance = velocity.y * Time.fixedDeltaTime;
-            RaycastHit2D hit2D = Physics2D.Raycast(raycastOrigin, raycastDirection, raycastDistance);
+            // Ground detection
+            Vector2 groundRaycastOrigin = new Vector2(pos.x, pos.y - 0.5f); // Adjust the origin to be at the player's feet
+            Vector2 groundRaycastDirection = Vector2.down;
+            float groundRaycastDistance = Mathf.Abs(velocity.y * Time.fixedDeltaTime);
+            RaycastHit2D groundHit2D = Physics2D.Raycast(groundRaycastOrigin, groundRaycastDirection, groundRaycastDistance);
 
-            if (hit2D.collider != null)
+            if (groundHit2D.collider != null)
             {
-                Platform platform = hit2D.collider.GetComponent<Platform>();
+                Platform platform = groundHit2D.collider.GetComponent<Platform>();
                 if (platform != null)
                 {
-                    // Adjust the player's position to the top of the platform's collider
-                    groundHeight = platform.GetComponent<Collider2D>().bounds.max.y;
-                    pos.y = groundHeight;
-                    velocity.y = 0;
-                    isGrounded = true;
+                    // Adjust the player's position to the top of the platform's collider only if falling
+                    if (velocity.y <= 0)
+                    {
+                        groundHeight = platform.GetComponent<Collider2D>().bounds.max.y;
+                        pos.y = groundHeight + 0.5f; // Adjust the position to be at the player's feet
+                        velocity.y = 0;
+                        isGrounded = true;
+                    }
                 }
             }
-            //Debug.DrawRay(raycastOrigin, raycastDirection * raycastDistance, Color.red);
+
+            // Wall detection
+            Vector2 wallRaycastOrigin = new Vector2(pos.x, pos.y);
+            Vector2 wallRaycastDirection = Vector2.right;
+            float wallRaycastDistance = Mathf.Abs(velocity.x * Time.fixedDeltaTime);
+            RaycastHit2D wallHit2D = Physics2D.Raycast(wallRaycastOrigin, wallRaycastDirection, wallRaycastDistance);
+
+            if (wallHit2D.collider != null)
+            {
+                Platform platform = wallHit2D.collider.GetComponent<Platform>();
+                if (platform != null)
+                {
+                    // Ensure the player falls if they hit the side of the platform
+                    if (pos.y < platform.GetComponent<Collider2D>().bounds.max.y)
+                    {
+                        Debug.Log("Hit wall");
+                        velocity.x = 0;
+                        velocity.y = -Mathf.Abs(velocity.y); // Ensure the player falls
+                    }
+                }
+                else
+                {
+                    velocity.x = 0;
+                }
+            }
         }
 
         distance += velocity.x * Time.fixedDeltaTime;
@@ -114,7 +154,6 @@ public class Player : MonoBehaviour
             {
                 isGrounded = false;
             }
-            //Debug.DrawRay(raycastOrigin, raycastDirection * raycastDistance, Color.green);
         }
 
         transform.position = pos;
--- Assets/Scripts/UI_Control.cs
+++ Assets/Scripts/UI_Control.cs
@@ -2,16 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UI_Control : MonoBehaviour
 {
     Player player;
     TextMeshProUGUI distanceText;
 
+    GameObject deathScreen;
+    TextMeshProUGUI distanceResult;
+
     private void Awake()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
         distanceText = GameObject.Find("DistanceText").GetComponent<TextMeshProUGUI>();
+
+        distanceResult = GameObject.Find("DistanceResult").GetComponent<TextMeshProUGUI>();
+
+        deathScreen = GameObject.Find("DeathScreen");
+        deathScreen.SetActive(false);
+
     }
 
     // Start is called before the first frame update
@@ -25,5 +36,21 @@ public class UI_Control : MonoBehaviour
     {
         int distance = Mathf.FloorToInt(player.distance);
         distanceText.text = distance + "m";
+
+        if (player.dead)
+        {
+            deathScreen.SetActive(true);
+            distanceResult.text = distance + "m";
+        }
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("Test Level");
     }
 }
